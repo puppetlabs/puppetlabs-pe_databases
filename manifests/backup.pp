f447b179e542cf7f0db4253c367c1625b65c745d
Nick Walker <nick.walker@puppetlabs.com>
2016-09-24 10:16:15 -0700

    Encapsulate backup into its own class
    Prior to this commit, the backup functionality was partially inside
    init.pp and partially in a defined type.
    
    After this commit, the backup functionality is in its own class
    pe_databases::backup.

--- manifests/backup.pp
+++ manifests/backup.pp
@@ -1,19 +1,60 @@
-define pe_databases::backup (
-  Array[String] $databases_to_backup = [ 'pe-activity', 'pe-classifier', 'pe-postgres', 'pe-puppetdb', 'pe-rbac', 'pe-orchestrator' ],
-  String $minute                     = '30',
-  String $hour                       = '23',
-  String $weekday                    = '*',
+class pe_databases::backup (
+  Array[Hash] $databases_and_backup_schedule = [
+    {
+      'databases' => ['pe-activity', 'pe-classifier', 'pe-postgres', 'pe-rbac', 'pe-orchestrator'],
+      'schedule'  =>
+      {
+        'minute' => '30',
+        'hour'   => '22',
+      },
+    },
+    {
+      'databases' => ['pe-puppetdb'],
+      'schedule'  =>
+      {
+        'minute'  => '0',
+        'hour'    => '2',
+        'weekday' => '7',
+      },
+    }
+  ],
+  String $backup_directory         = '/opt/puppetlabs/server/data/postgresql/9.4/backups',
+  String $backup_script_path       = '/opt/puppetlabs/pe_databases/scripts/puppet_enterprise_database_backup.sh',
+  String $backup_logging_directory = '/var/log/puppetlabs/pe_databases_backup',
 ) {
 
-  $db_string = join($databases_to_backup, " ")
+  ensure_resource( 'file', [ '/opt/puppetlabs/pe_databases', '/opt/puppetlabs/pe_databases/scripts', $backup_directory ],
+    { 'ensure' => 'directory' }
+  )
 
-  cron { "puppet_enterprise_database_backup_${databases_to_backup}":
-    ensure  => present,
-    command => "${pe_databases::backup_script_path} -l ${pe_databases::backup_logging_directory} -t ${pe_databases::backup_directory} ${db_string}",
-    user    => 'pe-postgres',
-    minute  => $minute,
-    hour    => $hour,
-    weekday => $weekday,
-    require => File['puppet_enterprise_db_backup_script'],
+  ensure_resource( 'file', $backup_logging_directory,
+    { 'ensure' => 'directory',
+      'owner' => 'pe-postgres',
+      'group' => 'pe-postgres', }
+  )
+
+  file { 'puppet_enterprise_db_backup_script' :
+    ensure => file,
+    owner  => 'pe-postgres',
+    group  => 'pe-postgres',
+    mode   => '0750',
+    path   => $backup_script_path,
+    source => 'puppet:///modules/pe_databases/puppet_enterprise_database_backup.sh',
+  }
+
+  $databases_and_backup_schedule.each | Hash $dbs_and_schedule | {
+
+    $databases_to_backup = $dbs_and_schedule['databases']
+    $db_string = join($databases_to_backup, " ")
+
+    cron { "puppet_enterprise_database_backup_${databases_to_backup}":
+      ensure  => present,
+      command => "${backup_script_path} -l ${backup_logging_directory} -t ${backup_directory} ${db_string}",
+      user    => 'pe-postgres',
+      minute  => $dbs_and_schedule['schedule']['minute'],
+      hour    => $dbs_and_schedule['schedule']['hour'],
+      weekday => $dbs_and_schedule['schedule']['weekday'],
+      require => File['puppet_enterprise_db_backup_script'],
+    }
   }
 }
--- manifests/init.pp
+++ manifests/init.pp
@@ -1,28 +1,7 @@
 class pe_databases (
-  Array[Hash] $databases_and_backup_schedule = [
-    {
-      'databases' => ['pe-activity', 'pe-classifier', 'pe-postgres', 'pe-rbac', 'pe-orchestrator'],
-      'schedule'  =>
-      {
-        'minute' => '30',
-        'hour'   => '22',
-      },
-    },
-    {
-      'databases' => ['pe-puppetdb'],
-      'schedule'  =>
-      {
-        'minute'  => '0',
-        'hour'    => '2',
-        'weekday' => '7',
-      },
-    }
-  ],
+  Boolean $manage_database_backups     = true,
   Boolean $manage_database_maintenance = true,
   Boolean $manage_postgresql_settings  = true,
-  String $backup_directory             = '/opt/puppetlabs/server/data/postgresql/9.4/backups',
-  String $backup_script_path           = '/opt/puppetlabs/pe_databases/scripts/puppet_enterprise_database_backup.sh',
-  String $backup_logging_directory     = '/var/log/puppetlabs/pe_databases_backup',
 ) {
 
   if $manage_database_maintenance {
@@ -33,34 +12,7 @@ class pe_databases (
     include pe_databases::postgresql_settings
   }
 
-  if !empty($databases_and_backup_schedule) {
-    ensure_resource( 'file', [ '/opt/puppetlabs/pe_databases', '/opt/puppetlabs/pe_databases/scripts', $backup_directory ],
-      { 'ensure' => 'directory' }
-    )
-
-    ensure_resource( 'file', $backup_logging_directory,
-      { 'ensure' => 'directory',
-         'owner' => 'pe-postgres',
-         'group' => 'pe-postgres', }
-    )
-
-
-    file { 'puppet_enterprise_db_backup_script' :
-      ensure => file,
-      owner  => 'pe-postgres',
-      group  => 'pe-postgres',
-      mode   => '0750',
-      path   => $backup_script_path,
-      source => 'puppet:///modules/pe_databases/puppet_enterprise_database_backup.sh',
-    }
-
-    $databases_and_backup_schedule.each | Hash $dbs_and_schedule | {
-      pe_databases::backup{ "${dbs_and_schedule['databases']}" :
-        databases_to_backup => $dbs_and_schedule['databases'],
-        minute              => $dbs_and_schedule['schedule']['minute'],
-        hour                => $dbs_and_schedule['schedule']['hour'],
-        weekday             => $dbs_and_schedule['schedule']['weekday'],
-      }
-    }
+  if $manage_database_backups {
+    include pe_databases::backup
   }
 }
