4381154d912d868fed225c3e69276c0513420404
Nick Walker <nick.walker@puppetlabs.com>
2017-10-05 16:26:00 -0700

    Make cronjobs to vacuum full puppetdb tables
    We want to VACUUM FULL all the tables except reports and
    resource events.  We do this across days and for some lesser used
    tables we do this only once per month.
    
    By default the script will pause for 5 minutes between VACUUM
    FULL of each table.  This allows the PuppetDB queue to work
    down from the last VACUUM FULL.
    
    Note that cronjob default times are chosen to note conflict with
    the database backup times in the backup manifest.

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,12 +2,18 @@ class pe_databases (
   Boolean $manage_database_backups     = true,
   Boolean $manage_database_maintenance = true,
   Boolean $manage_postgresql_settings  = true,
+  String  $install_dir                 = '/opt/puppetlabs/pe_databases',
+  String  $scripts_dir                 = "${install_dir}/scripts"
 ) {
 
   if ( versioncmp('2017.3.0', $facts['pe_server_version']) <= 0 ) {
     $psql_version = '9.6'
   } else {
     $psql_version = '9.4'
+  }
+
+  file { [$install_dir, $scripts_dir] :
+    ensure => directory,
   }
 
   if $manage_database_maintenance {
--- manifests/maintenance.pp
+++ manifests/maintenance.pp
@@ -3,7 +3,8 @@ class pe_databases::maintenance (
   Integer $maint_cron_weekday = 6,
   Integer $maint_cron_hour    = 1,
   Integer $maint_cron_minute  = 0,
-  String  $logging_directory  = '/var/log/puppetlabs/pe_databases_cron'
+  String  $logging_directory  = '/var/log/puppetlabs/pe_databases_cron',
+  String  $script_directory   = $pe_databases::scripts_dir,
 ){
 
   $ensure_cron = $disable_maintenace ? {
@@ -15,14 +16,54 @@ class pe_databases::maintenance (
     ensure => directory,
   }
 
+  $vacuum_script_path = "${script_directory}/vacuum_full_tables.sh"
+
+  file { $vacuum_script_path:
+    ensure => file,
+    source => 'puppet:///modules/pe_databases/vacuum_full_tables.sh',
+    owner  => 'pe-postgres',
+    group  => 'pe-postgres',
+    mode   => '744',
+  }
+
+  cron { 'VACUUM FULL facts tables' :
+    ensure   => $ensure_cron,
+    user     => 'root',
+    weekday => [2,6],
+    hour     => 4,
+    minute   => 30,
+    command  => "${vacuum_script_path} facts > ${logging_directory}/facts_output.log 2>| tee ${logging_directory}/facts_error.log",
+    require  => File[$logging_directory, $script_directory],
+  }
+
+  cron { 'VACUUM FULL catalogs tables' :
+    ensure   => $ensure_cron,
+    user     => 'root',
+    weekday  => [0,4],
+    hour     => 4,
+    minute   => 30,
+    command  => "${vacuum_script_path} catalogs > ${logging_directory}/catalogs_output.log 2>| tee ${logging_directory}/catalogs_error.log",
+    require  => File[$logging_directory, $script_directory],
+  }
+
+  cron { 'VACUUM FULL other tables' :
+    ensure   => $ensure_cron,
+    user     => 'root',
+    monthday => 20,
+    hour     => 5,
+    minute   => 30,
+    command  => "${vacuum_script_path} other > ${logging_directory}/other_output.log 2>| tee ${logging_directory}/other_error.log",
+    require  => File[$logging_directory, $script_directory],
+  }
+
+  #Remove old versions of maintenance cron jobs
   cron { 'Maintain PE databases' :
-    ensure  => $ensure_cron,
+    ensure  => absent,
     user    => 'root',
     weekday => $maint_cron_weekday,
     hour    => $maint_cron_hour,
     minute  => $maint_cron_minute,
     command => "su - pe-postgres -s /bin/bash -c '/opt/puppetlabs/server/bin/reindexdb --all; /opt/puppetlabs/server/bin/vacuumdb --analyze --verbose --all' > ${logging_directory}/output.log 2> ${logging_directory}/output_error.log",
-    require => File[$logging_directory],
+    require => File[$logging_directory, $script_directory],
   }
-
 }
