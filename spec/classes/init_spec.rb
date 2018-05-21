3669ba839479909e19f3563fd5213470079a9b1c
Nick Walker <nick.walker@puppetlabs.com>
2018-05-21 09:45:00 -0700

    PDK Update - minimum spec test

---
+++ spec/classes/init_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+
+describe 'pe_databases' do
+  let(:params) do
+    {}
+  end
+
+  on_supported_os.each do |os, os_facts|
+    context "on #{os}" do
+      let(:facts) { os_facts }
+
+      it { is_expected.to compile }
+    end
+  end
+end
