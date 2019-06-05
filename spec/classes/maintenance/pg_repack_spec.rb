9d91872b8fdbc5f790883afb9104bc81e02fc38f
Nick Walker <nick.walker@puppetlabs.com>
2019-06-05 10:50:48 -0700

    Add resource_events table to pg_repack
    Add jobs parameter to pg_repack

--- spec/classes/maintenance/pg_repack_spec.rb
+++ spec/classes/maintenance/pg_repack_spec.rb
@@ -7,6 +7,36 @@ describe 'pe_databases::maintenance::pg_repack' do
       let(:facts) { os_facts }
 
       it { is_expected.to compile }
+      context 'on PE 2019.1.0' do
+        before :each do
+          facts['pe_server_version'] = '2019.1.0'
+        end
+        it {
+          is_expected.to contain_cron('pg_repack reports tables')
+            .with_command('su - pe-postgres -s /bin/bash -c "/opt/puppetlabs/server/apps/postgresql/bin/pg_repack'\
+              ' -d pe-puppetdb --jobs 2 -t reports -t resource_events" > /var/log/puppetlabs/pe_databases_cron/output.log 2>&1')
+        }
+      end
+      context 'on > PE 2019.1.0' do
+        before :each do
+          facts['pe_server_version'] = '2019.2.0'
+        end
+        it {
+          is_expected.to contain_cron('pg_repack reports tables')
+            .with_command('su - pe-postgres -s /bin/bash -c "/opt/puppetlabs/server/apps/postgresql/bin/pg_repack'\
+              ' -d pe-puppetdb --jobs 2 -t reports -t resource_events" > /var/log/puppetlabs/pe_databases_cron/output.log 2>&1')
+        }
+      end
+      context 'on < PE 2019.1.0' do
+        before :each do
+          facts['pe_server_version'] = '2019.0.0'
+        end
+        it {
+          is_expected.to contain_cron('pg_repack reports tables')
+            .with_command('su - pe-postgres -s /bin/bash -c "/opt/puppetlabs/server/apps/postgresql/bin/pg_repack'\
+              ' -d pe-puppetdb --jobs 2 -t reports" > /var/log/puppetlabs/pe_databases_cron/output.log 2>&1')
+        }
+      end
     end
   end
 end
