fc836a022947a2d568f82c215a47f08f26ab8823
tkishel <tkishel@gmail.com>
2019-10-29 15:56:53 -0700

    (PDB-2487) add spec tests

--- spec/classes/maintenance/manage_resource_events_spec.rb
+++ spec/classes/maintenance/manage_resource_events_spec.rb
@@ -8,5 +8,49 @@ describe 'pe_databases::maintenance::manage_resource_events' do
 
       it { is_expected.to compile }
     end
+
+    let(:pre_condition) do
+      <<-MANIFEST
+        file { '/opt/puppetlabs/pe_databases/scripts':  ensure => 'directory' }
+        file { '/var/log/puppetlabs/pe_databases_cron': ensure => 'directory' }
+      MANIFEST
+    end
+
+    context 'with resource_events_ttl => 0' do
+      let(:params) do
+        {
+          'disable_maintenance' => false,
+          'resource_events_ttl' => 0,
+          'script_directory'    => '/opt/puppetlabs/pe_databases/scripts',
+          'logging_directory'   => '/var/log/puppetlabs/pe_databases_cron',
+        }
+      end
+
+      it { is_expected.to contain_cron('DELETE FROM resource_events').with_ensure('absent') }
+    end
+    context 'with resource_events_ttl => 3' do
+      let(:params) do
+        {
+          'disable_maintenance' => true,
+          'resource_events_ttl' => 3,
+          'script_directory'    => '/opt/puppetlabs/pe_databases/scripts',
+          'logging_directory'   => '/var/log/puppetlabs/pe_databases_cron',
+        }
+      end
+
+      it { is_expected.to contain_cron('DELETE FROM resource_events').with_ensure('absent') }
+    end
+    context 'with resource_events_ttl => 3' do
+      let(:params) do
+        {
+          'disable_maintenance' => false,
+          'resource_events_ttl' => 3,
+          'script_directory'    => '/opt/puppetlabs/pe_databases/scripts',
+          'logging_directory'   => '/var/log/puppetlabs/pe_databases_cron',
+        }
+      end
+
+      it { is_expected.to contain_cron('DELETE FROM resource_events').with_ensure('present') }
+    end
   end
 end
