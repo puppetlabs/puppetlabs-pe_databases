88ef7f0cf424241f6d5ca2f61248f54a86779ffd
tkishel <tkishel@gmail.com>
2020-03-13 12:28:35 -0700

    (maint) update to pass shellcheck and correct versions
    Correct shellcheck warnings
    Remove hard-coded reference to 9.4.

--- files/puppet_enterprise_database_backup.sh
+++ files/puppet_enterprise_database_backup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-BACKUPDIR=/opt/puppetlabs/server/data/postgresql/9.4/backups
+PG_VERSION=$(/usr/local/bin/facter -p pe_postgresql_info.installed_server_version)
+BACKUPDIR=/opt/puppetlabs/server/data/postgresql/$PG_VERSION/backups
 LOGDIR=/var/log/puppetlabs/pe_databases_backup
 RETENTION=2
 
@@ -26,35 +27,39 @@ while [[ $# -gt 0 ]]; do
   esac
 done
 
-let RETENTION_ENFORCE=$RETENTION-1
-
 if [[ -z "${DATABASES}" ]]; then
-  echo "Usage: $0 [-l LOG_DIRECTORY] [-t BACKUP_TARGET] [-r RETENTION] <DATABASE1> [DATABASE...]"
+  echo "Usage: $0  [-t BACKUP_TARGET] [-l LOG_DIRECTORY] [-r RETENTION] <DATABASE> [DATABASE_N ...]"
   exit 1
 fi
 
+RETENTION_ENFORCE=$((RETENTION-1))
+
 for db in $DATABASES; do
-  echo "Enforcing retention policy of storing only ${RETENTION_ENFORCE} backups for ${db}" >> ${LOGDIR}/${db}.log 2>&1
+  echo "Enforcing retention policy of storing only ${RETENTION_ENFORCE} backups for ${db}" >> "${LOGDIR}/${db}.log" 2>&1
 
   ls -1tr ${BACKUPDIR}/${db}_* | head -n -${RETENTION_ENFORCE} | xargs -d '\n' rm -f --
 
-  echo "Starting dump of database: ${db}" >> ${LOGDIR}/${db}.log 2>&1
+  echo "Starting dump of database: ${db}" >> "${LOGDIR}/${db}.log" 2>&1
 
-  if [ ${db} == "pe-classifier" ]; then
-    #Save space before backing up by clearing unused node_check_ins table
-    /opt/puppetlabs/server/bin/psql -d pe-classifier -c 'TRUNCATE TABLE node_check_ins' >> ${LOGDIR}/${db}.log 2>&1
-    if [ $? != 0 ]; then
-      echo "Failed to truncate node_check_ins table." >> ${LOGDIR}/${db}.log 2>&1
+  if [ "${db}" == "pe-classifier" ]; then
+    # Save space before backing up by clearing unused node_check_ins table.
+    /opt/puppetlabs/server/bin/psql -d pe-classifier -c 'TRUNCATE TABLE node_check_ins' >> "${LOGDIR}/${db}.log" 2>&1
+
+    result=$?
+    if [ $result != 0 ]; then
+      echo "Failed to truncate node_check_ins table" >> "${LOGDIR}/${db}.log" 2>&1
     fi
   fi
 
-  /opt/puppetlabs/server/bin/pg_dump -Fc ${db} -f ${BACKUPDIR}/${db}_`date +%m_%d_%y_%H_%M`.bin >> ${LOGDIR}/${db}.log 2>&1
+  DATETIME=$(date +%m_%d_%y_%H_%M)
+
+  /opt/puppetlabs/server/bin/pg_dump -Fc "${db}" -f "${BACKUPDIR}/${db}_$DATETIME.bin" >> "${LOGDIR}/${db}.log" 2>&1
 
   result=$?
   if [[ $result -eq 0 ]]; then
-    echo "Completed dump of database: ${db}" >> ${LOGDIR}/${db}.log 2>&1
+    echo "Completed dump of database: ${db}" >> "${LOGDIR}/${db}.log" 2>&1
   else
-    echo "Failed to dump database ${db}. Exit code is: ${result}" >> ${LOGDIR}/${db}.log 2>&1
+    echo "Failed to dump database ${db}. Exit code is: ${result}" >> "${LOGDIR}/${db}.log" 2>&1
     exit 1
   fi
 done
