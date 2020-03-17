acb1f9f9fe09ab7893e52c00cbe6260919a6f0d1
Adrian <sensei.loafage@gmail.com>
2020-03-17 15:01:50 -0700

    Refactor shell scripts
    Use a more sensible timestamp for backups
    Use arrays when possible
    Lowercase variables
    More safely process backup retention

--- files/vacuum_full_tables.sh
+++ files/vacuum_full_tables.sh
@@ -1,39 +1,43 @@
 #!/bin/bash
 
-if [ "$1" = "" ]; then
-  echo "Usage: $0 <(facts, catalogs, or other) tables to VACUUM FULL> "
-  exit
-fi
+usage() {
+  cat <<EOF
+echo "usage: $0 <table>"
+where <table> is one of: facts catalogs other
+EOF
+  exit 1
+}
+# Print usage if given 0 arguments
+(( $# == 0 )) && usage
 
-if [ "$2" = "" ]; then
-  SLEEP=300
-else
-  SLEEP=$2
-fi
+sleep_duration="${2:-300}"
 
 # TODO: Is this used in PE 2018 and newer? RE: fact_values
+case "$1" in
+  'facts')
+    vacuum_tables=("'facts'" "'factsets'" "'fact_paths'" "'fact_values'")
+    ;;
+  'catalogs')
+    vacuum_tables=("'catalogs'" "'catalog_resources'" "'edges'" "'certnames'")
+    ;;
+  'other')
+    vacuum_tables=("'producers'" "'resource_params'" "'resource_params_cache'")
+    ;;
+  *)
+    usage
+esac
 
-if [ "$1" = 'facts' ]; then
-  WHERE="'facts', 'factsets', 'fact_paths', 'fact_values'"
-elif [ "$1" = 'catalogs' ]; then
-  WHERE="'catalogs', 'catalog_resources', 'edges', 'certnames'"
-elif [ "$1" = 'other' ]; then
-  WHERE="'producers', 'resource_params', 'resource_params_cache'"
-else
-  echo "Must pass facts, catalogs, or other as first argument"
-  exit 1
-fi
 
 SQL="SELECT t.relname::varchar AS table_name
   FROM pg_class t
   JOIN pg_namespace n
     ON n.oid = t.relnamespace
   WHERE t.relkind = 'r'
-    AND t.relname IN ( $WHERE )"
+    AND t.relname IN ( $(IFS=,; echo "${vacuum_tables[*]}") )"
 
-for TABLE in $(su - pe-postgres -s /bin/bash -c "/opt/puppetlabs/server/bin/psql -d pe-puppetdb -c \"$SQL\" --tuples-only")
-do
-  # echo "$TABLE"
-  su - pe-postgres -s /bin/bash -c "/opt/puppetlabs/server/bin/vacuumdb -d pe-puppetdb -t $TABLE --full --analyze"
-  sleep "$SLEEP"
+tables=($(su - pe-postgres -s /bin/bash -c "/opt/puppetlabs/server/bin/psql -d pe-puppetdb -c \"$SQL\" --tuples-only"))
+
+for table in "${tables[@]}"; do
+  su - pe-postgres -s /bin/bash -c "/opt/puppetlabs/server/bin/vacuumdb -d pe-puppetdb -t $table --full --analyze"
+  sleep "$sleep_duration"
 done
