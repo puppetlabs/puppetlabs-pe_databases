f45468fc9584a41a1bdb8add1c5f9781e17e8460
Adrian Parreiras Horta <adrian.parreiras-horta@puppet.com>
2020-03-18 08:47:43 -0700

    Shellcheck fixes

--- files/puppet_enterprise_database_backup.sh
+++ files/puppet_enterprise_database_backup.sh
@@ -28,6 +28,8 @@ done
 
 # The remaining parameters will be the databases to backup
 databases=("$@")
+# shellcheck disable=SC2128
+# We only care if the array contains any elements
 [[ $databases ]] || {
   echo "Usage: $0  [-t BACKUP_TARGET] [-l LOG_DIRECTORY] [-r retention] <DATABASE> [DATABASE_N ...]"
   exit 1
@@ -39,7 +41,7 @@ log_dir="${log_dir:-/var/log/puppetlabs/pe_databases_backup}"
 retention="${retention:-2}"
 
 for db in "${databases[@]}"; do
-  # For each db, redirect all output the the log inside the backup dir
+  # For each db, redirect all output to the log inside the backup dir
   exec &>"${log_dir}/${db}.log"
   echo "Enforcing retention policy of storing only $retention backups for $db"
 
@@ -48,13 +50,13 @@ for db in "${databases[@]}"; do
   # Add the filename to the backups array, giving us a sorted list of filenames
   while IFS= read -r -d '' line; do
     backups+=("${line#* }")
-  done < <(stat --printf '%Y %n\0' ${backup_dir}/${db}_* 2>/dev/null | sort -nz)
+  done < <(stat --printf '%Y %n\0' "${backup_dir}/${db}_"* 2>/dev/null | sort -nz)
 
   # Our array offset will be the number of backups - $retention + 1
   # e.g. if we have 2 existing backups and retention=2, offset will be one
   # We'll delete from element 0 to 1 of the array, leaving one backup.
   # The subsequent backup will leave us with 2 again
-  offset=$(( ${#backups[@]} - $retention + 1 ))
+  offset=$(( ${#backups[@]} - retention + 1 ))
 
   if (( offset > 0 )); then
   # Continue if we're retaining more copies of the db than currently exist
