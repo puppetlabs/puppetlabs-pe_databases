280165280f689d8b26bb07ec142a99c76ba90779
martyewings <martyewings@gmail.com>
2021-04-27 16:18:35 +0100

    set pe_databases defaults

--- spec/classes/maintenance/vacuum_full_spec.rb
+++ spec/classes/maintenance/vacuum_full_spec.rb
@@ -3,7 +3,6 @@ require 'spec_helper'
 describe 'pe_databases::maintenance::vacuum_full' do
   on_supported_os.each do |os, os_facts|
     context "on #{os}" do
-      let(:pre_condition) { "class { 'pe_databases': }" }
       let(:facts) { os_facts }
 
       it { is_expected.to compile }
