3a52625db9303d1f297572f8d949683745e62f15
paddy <patrick.grant@puppet.com>
2021-05-07 17:58:41 +0100

    Add some error checking for the PE-PostgreSQL service

--- tasks/reset_pgrepack_schema.sh
+++ tasks/reset_pgrepack_schema.sh
@@ -2,10 +2,15 @@
 
 # Puppet Task Name: reset_pgrepack_schema
 
-#Remove the pg_repack extension
-su - pe-postgres -s '/bin/bash' -c '/opt/puppetlabs/server/bin/psql -d pe-puppetdb -c "DROP EXTENSION pg_repack CASCADE"'
+#Determine if PE Postgres is available
+if puppet resource service pe-postgresql | grep -q running; then
+    #Remove the pg_repack extension
+    su - pe-postgres -s '/bin/bash' -c '/opt/puppetlabs/server/bin/psql -d pe-puppetdb -c "DROP EXTENSION pg_repack CASCADE"'
 
-#Then, recreate the pg_repack extension
-su - pe-postgres -s '/bin/bash' -c '/opt/puppetlabs/server/bin/psql -d pe-puppetdb -c "CREATE EXTENSION pg_repack"'
+    #Then, recreate the pg_repack extension
+    su - pe-postgres -s '/bin/bash' -c '/opt/puppetlabs/server/bin/psql -d pe-puppetdb -c "CREATE EXTENSION pg_repack"'
+else
+  echo " -- No running PE-PostgreSQL instance found, please run this task against your Primary or PE-PostgreSQL server --"
+fi
 
 echo " -- reset_pgrepack_scheme Task ended: $(date +%s) --"
