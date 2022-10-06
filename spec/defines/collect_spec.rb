1e3a55161296e3a550709cd18ecbc1dead3b6ef4
Adrian <sensei.loafage@gmail.com>
2022-10-06 09:50:44 -0700

    Add tests for ensure absent, notify, and subscribe

--- spec/defines/collect_spec.rb
+++ spec/defines/collect_spec.rb
@@ -19,6 +19,13 @@ describe 'pe_databases::collect' do
       is_expected.to contain_file('/etc/systemd/system/pe_databases-test.service').with_content(
         %r{ExecStart=foo},
       )
+
+      is_expected.to contain_service('pe_databases-test.service').that_notifies(
+        'Exec[pe_databases_daemon_reload]',
+      )
+      is_expected.to contain_service('pe_databases-test.timer').that_subscribes_to(
+        'File[/etc/systemd/system/pe_databases-test.timer]',
+      )
     }
   end
 
@@ -34,6 +41,8 @@ describe 'pe_databases::collect' do
     end
 
     it {
+      is_expected.to contain_file('/etc/systemd/system/pe_databases-test.timer').with_ensure('absent')
+      is_expected.to contain_file('/etc/systemd/system/pe_databases-test.service').with_ensure('absent')
       is_expected.to contain_service('pe_databases-test.timer').with_ensure('stopped')
     }
   end
